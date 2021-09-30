40220735bc0bf59aa406d1964ec5873364d05dc0
Vadim <komkowvadim@gmail.com>
2021-09-30 03:47:16 +0300

    Дз по блоку "Основы"

--- state_homework/lib/main.dart
+++ state_homework/lib/main.dart
@@ -7,23 +7,94 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "My App",
+      home: Scaffold(
+        backgroundColor: Colors.indigo[300],
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "Counter App",
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+            ),
+          ),
         ),
-      ),
-      body: Center(
-        child: Container(
+        body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: [],
+            children: [
+              Text(
+                "Tap \"+\" to increment",
+                style: TextStyle(color: Colors.white),
+              ),
+              Counter(),
+              Text(
+                "Tap \"-\" to decrement",
+                style: TextStyle(color: Colors.white),
+              ),
+            ],
           ),
         ),
       ),
-    ));
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CounterState();
+  }
+}
+
+class _CounterState extends State<Counter> {
+  int _counterValue = 0;
+  @override
+  void initState() {
+    _counterValue = 50;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //color: Colors.white,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.blueGrey,
+          width: 3,
+        ),
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+      ),
+      constraints: BoxConstraints(
+        maxWidth: MediaQuery.of(context).size.width * 0.3,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  _counterValue++;
+                });
+              },
+              icon: Icon(Icons.add)),
+          Text(
+            "$_counterValue",
+            style: TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          IconButton(
+              onPressed: () {
+                setState(() {
+                  _counterValue--;
+                });
+              },
+              icon: Icon(Icons.remove)),
+        ],
+      ),
+    );
   }
 }
